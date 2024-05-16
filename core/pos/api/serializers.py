fc62bb7ed0df556297597e4da6052dabd770ef4e
JefferDevop <somadesarrollo@gmail.com>
2024-05-15 21:38:58 -0500

    My first commit

---
+++ core/pos/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework.serializers import ModelSerializer
+from core.pos.models import Category, Product, Provider
+
+
+
+
+class ProviderSerializer(ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ["id", "name", "ruc", "mobile", "email", "address"]
+
+
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "slug", "image_alterna"]
+
+
+class ProductSerializer(ModelSerializer):
+    # atributData = AttributSerializer(source='atribut', read_only=True, many=True)
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "code",
+            "ref",
+            "flag",
+            "name",
+            "slug",
+            "description",          
+            "category",
+            "image",        
+            "image_alterna",
+            "pvp",
+            "price",
+            "stock",      
+            "active",
+            "soldout",
+            "offer",
+            "home",       
+        ]
